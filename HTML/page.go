b0978f22abf4978a8845d4f53f104528fcdb2a03
omkhalid <omark.janahi@gmail.com>
2024-05-15 19:44:52 +0300

    adding to Github

--- HTML/page.go
+++ HTML/page.go
@@ -4,6 +4,8 @@ import (
 	"encoding/json"
 	"html/template"
 	"net/http"
+	"strconv"
+	// "strings"
 )
 
 type HomePageData struct {
@@ -18,25 +20,42 @@ type Artist struct {
 	CreationDate int      `json:"creationDate"`
 	FirstAlbum   string   `json:"firstAlbum"`
 	Locations    string   `json:"locations"`
-	ConcertDates string   `json:"concertDates"`
-	Relations    string   `json:"relations"`
+	ConcertDates string   `json:"dates"`
+	Relations    string   `json:"datesLocations"`
 }
-
 type Location struct {
 	ID        int      `json:"id"`
-	Locations []string `json:"locations"`
+	LocationS []string `json:"locations"`
 }
-
-type Dates struct {
-	ID    int      `json:"id"`
-	Dates []string `json:"dates"`
+type Date struct {
+	ID           int      `json:"id"`
+	ConcertDates []string `json:"dates"`
+}
+type Relation struct {
+	ID             int                 `json:"id"`
+	DatesLocations map[string][]string `json:"datesLocations"`
+}
+type ArtistWithInfo struct {
+	Artist
+	Locations []string            `json:"locations"`
+	Dates     []string            `json:"dates"`
+	Relations map[string][]string `json:"datesLocations"`
 }
 
 func HandleRequest(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" && r.URL.Path != "/index.html" {
+		HandleNotFound(w,r)
+		return
+	}
+	if r.Method != http.MethodGet{
+		HandleMethod(w,r)
+		return
+	}
 	// this is where we are getting the API info
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists")
 	if err != nil {
-		http.Error(w, "Failed to fetch artist data", http.StatusInternalServerError)
+		HandleInternalError(w,r)
+
 		return
 	}
 	defer resp.Body.Close()
@@ -45,14 +64,14 @@ func HandleRequest(w http.ResponseWriter, r *http.Request) {
 	var artists []Artist
 	err = json.NewDecoder(resp.Body).Decode(&artists)
 	if err != nil {
-		http.Error(w, "Failed to decode artist data", http.StatusInternalServerError)
+		HandleInternalError(w,r)
 		return
 	}
 
-	// this is where it is recognizing the imdex.html
+	// this is where it is recognizing the index.html
 	tmpl, err := template.ParseFiles("templates/index.html")
 	if err != nil {
-		http.Error(w, "Failed to parse template", http.StatusInternalServerError)
+		HandleInternalError(w,r)
 		return
 	}
 
@@ -68,48 +87,61 @@ func HandleRequest2(w http.ResponseWriter, r *http.Request) {
 	// Extract the artist ID from the query parameters
 	artistID := r.URL.Query().Get("id")
 
+	// Convert the artistID to an integer
+	id, err := strconv.Atoi(artistID)
+	if err != nil {
+		HandleNotFound(w,r)
+		return
+	}
+
+	// Check if the ID is greater than 52
+	if id > 52 || id <= 0 {
+		HandleNotFound(w,r)
+		return
+	}
+
 	// Fetch the artist's detailed information using the artist ID
 	resp, err := http.Get("https://groupietrackers.herokuapp.com/api/artists/" + artistID)
 	if err != nil {
-		http.Error(w, "Failed to fetch artist data", http.StatusInternalServerError)
+		HandleInternalError(w,r)
 		return
 	}
 	defer resp.Body.Close()
-
-	resploc, err2 := http.Get("https://groupietrackers.herokuapp.com/api/locations" + artistID)
-	if err2 != nil {
-		http.Error(w, "Failed to fetch artist data", http.StatusInternalServerError)
-		return
-	}
-	defer resploc.Body.Close()
-
-	respdate, err3 := http.Get("https://groupietrackers.herokuapp.com/api/dates" + artistID)
-	if err3 != nil {
-		http.Error(w, "Failed to fetch artist data", http.StatusInternalServerError)
-		return
-	}
-	defer respdate.Body.Close()
 
 	// Decode the artist's detailed information
 	var artist Artist
 	err = json.NewDecoder(resp.Body).Decode(&artist)
 	if err != nil {
-		http.Error(w, "Failed to decode artist data", http.StatusInternalServerError)
+		http.Error(w, "Failed to decode artist data", http.StatusBadGateway)
 		return
+	}
+
+	// Get the location information
+	location := getLocation(artist.ID, w, r)
+	// Get the relation information
+	relation := getRelation(artist.ID, w, r)
+	// Get the relation information
+	concertDate := getDates(artist.ID, w, r)
+
+	// Create a new instance of ArtistWithInfo
+	artistWithInfo := ArtistWithInfo{
+		Artist:    artist,
+		Locations: location.LocationS,
+		Dates:     concertDate.ConcertDates,
+		Relations: relation.DatesLocations,
 	}
 
 	// Load the info.html template
 	tmpl, err := template.ParseFiles("templates/info.html")
 	if err != nil {
-		http.Error(w, "Failed to parse template", http.StatusInternalServerError)
+		HandleInternalError(w,r)
 		return
 	}
 
 	// Execute the template with the artist's detailed information
-	err = tmpl.Execute(w, artist)
+	err = tmpl.Execute(w, artistWithInfo)
 	if err != nil {
-		http.Error(w, "Failed to render template", http.StatusInternalServerError)
+		HandleInternalError(w,r)
 		return
 	}
-
 }
--- main.go
+++ main.go
@@ -4,15 +4,19 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
 	never "never/HTML"
 )
 
 func main() {
 	http.HandleFunc("/", never.HandleRequest)
-	http.HandleFunc("/info.html", never.HandleRequest2)
+	http.HandleFunc("/artist", never.HandleRequest2)
 	http.Handle("/templates/", http.StripPrefix("/templates/", http.FileServer(http.Dir("templates/"))))
 	http.Handle("/HTML/", http.StripPrefix("/HTML/", http.FileServer(http.Dir("HTML/"))))
-	log.Fatal(http.ListenAndServe(":8080", nil))
-	fmt.Println("go to -->  http://localhost:8080/ ")
+	log.Fatal(http.ListenAndServe(":5500", nil))
+	fmt.Println("go to -->  http://localhost:5500/ ")
 
+	if err := http.ListenAndServe(":5500", nil); err != nil {
+		log.Fatal(err)
+	}
 }
